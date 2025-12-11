d2fdbf9962d8b16e4091289f61110af2d9f4ab90
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-12-11 16:03:09 -0500

    🔴 Generate_WithTwo_ReturnsListWith1And2

--- katas/kata-1/tests/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
+++ katas/kata-1/tests/FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
@@ -15,7 +15,17 @@ namespace FizzBuzz.Tests
             result.Should().HaveCount(1);
             result[0].Should().Be("1");
         }
-        
+
+        [Fact]
+        public void Generate_WithTwo_ReturnsListWith1And2()
+        {
+            var result = FizzBuzzGenerator.Generate(2);
+
+            result.Should().HaveCount(2);
+            result[0].Should().Be("1");
+            result[1].Should().Be("2");
+        }
+
         [Fact]
         public void Generate_WithFifteen_ReturnsListWithFizzBuzz()
         {
