0d4ced8ecf2298d6712fa8c184f2b586ed8d7f81
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-12-11 15:50:47 -0500

    🔴 Generate_WithOne_ReturnsListWithOne

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -5,7 +5,6 @@ namespace FizzBuzz
         public static List<string> Generate(int count)
         {
             // TODO: Implement FizzBuzz logic using TDD
-            // Start with the simplest case that makes the first test pass
             throw new NotImplementedException("Implement using TDD: Red → Green → Refactor");
         }
     }
