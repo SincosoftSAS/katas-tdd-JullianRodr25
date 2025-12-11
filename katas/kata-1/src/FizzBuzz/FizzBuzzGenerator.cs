85b7f7fb6650f836ced45db37d357bc984a80ca4
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-12-11 16:11:04 -0500

    🟢 Generate_WithFive_ReturnsBuzzAtFifthPosition

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -4,15 +4,18 @@ namespace FizzBuzz
     {
         public static List<string> Generate(int count)
         {
-            var  resultList = new List<string>();
+            var resultList = new List<string>();
 
             for (var i = 1; i <= count; i++)
             {
                 if (i == 3)
                     resultList.Add("Fizz");
+                else if (i == 5)
+                    resultList.Add("Buzz");
                 else
                     resultList.Add(i.ToString());
             }
+
             return resultList;
         }
     }
