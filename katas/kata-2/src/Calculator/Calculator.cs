dd77816e6ab81d2957b85f5a537095f6afe4b0d4
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-12-12 15:28:45 -0500

    🔵 Divide_TwoPositiveNumbers_ReturnsQuotient

--- katas/kata-2/src/Calculator/Calculator.cs
+++ katas/kata-2/src/Calculator/Calculator.cs
@@ -17,9 +17,9 @@ namespace Calculator
             return valor1 * valor2;
         }
 
-        public int Divide(int a, int b)
+        public int Divide(int valor1, int valor2)
         {
-            return a / b;
+            return valor1 / valor2;
         }
     }
 }
