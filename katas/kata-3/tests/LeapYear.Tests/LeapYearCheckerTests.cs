8e63dfbd746d478ab303e071ba01c7c6a91fa6aa
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-12-16 10:16:03 -0500

    🔴Year_DivisibleBy100_ButNotBy400_IsNotLeapYear

--- katas/kata-3/tests/LeapYear.Tests/LeapYearCheckerTests.cs
+++ katas/kata-3/tests/LeapYear.Tests/LeapYearCheckerTests.cs
@@ -17,5 +17,12 @@ namespace LeapYear.Tests
             // Ejemplo: 2001 no es año bisiestoo
             Assert.False(LeapYearChecker.IsLeapYear(2001));
         }
+        
+        [Fact]
+        public void Year_DivisibleBy100_ButNotBy400_IsNotLeapYear()
+        {
+            // Ejemplo: 1900 no es año bisiesto
+            Assert.False(LeapYearChecker.IsLeapYear(1900));
+        }
     }
 }
